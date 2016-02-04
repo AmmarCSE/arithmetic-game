e859ecbfbb2d366ba11820bd9803f900a1410783
AmmarCSE <ammar.abu-yasein@farsightsoftware.com>
2016-02-04 12:04:35 +0200

    set game logic questioning flow and options

--- expression-engine.js
+++ expression-engine.js
@@ -1,128 +1,142 @@
-module.exports = (function(){
-var expressionLevel = 3;
+module.exports = (function() {
+    var expressionLevel = 3;
+    var operators = ['+', '-', '*', '/'];
 
-function getRandomArrayItem(array) {
-  return array[Math.floor(Math.random()*array.length)];
-}
+    function getRandomArrayItem(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    }
 
-function getRandomOperator(){
-  var operators = ['+', '-', '*', '/'];
-  return getRandomArrayItem(operators);
-}
+    function getRandomOperator() {
+        return getRandomArrayItem(operators);
+    }
 
-function getRandomOperand(min, max) {
-  //set defaults customized for our game
-  min = min || 1;
-  max = max || 100;
+    function getRandomOperand(min, max) {
+        //set defaults customized for our game
+        min = min || 1;
+        max = max || 100;
 
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
-function insertRandomParanthesis(expressionArray) {
-    //give %50 chance of inserting paranthesis
-    if(1 || getRandomOperand() > 50){
-        //determine how many operands to include within the parenthesis
-        var minOperands = 2;
-        var maxOperands = expressionLevel - 1;
-        var includedOperands = getRandomOperand(minOperands, maxOperands);
-        
-        //have to initialize array with even numbers for valid start indices
-        var startIndexArray = Array.apply(null, {length: expressionLevel - 1}).map(function(item, index){ return index*2});
-        var randomStartIndex = getRandomArrayItem(startIndexArray);
- 
-        var endingIndexJump = randomStartIndex + ((includedOperands-1)*2)+1;
+    function insertRandomParanthesis(expressionArray) {
+        //give %50 chance of inserting paranthesis
+        if (1 || getRandomOperand() > 50) {
+            //determine how many operands to include within the parenthesis
+            var minOperands = 2;
+            var maxOperands = expressionLevel - 1;
+            var includedOperands = getRandomOperand(minOperands, maxOperands);
 
-        //check for and fix ending index surpassing array end
-        while(endingIndexJump > expressionArray.length){
-            //shift both down
-            randomStartIndex -= 2;
-            endingIndexJump -= 2;
+            //have to initialize array with even numbers for valid start indices
+            var startIndexArray = Array.apply(null, {
+                length: expressionLevel - 1
+            }).map(function(item, index) {
+                return index * 2
+            });
+            var randomStartIndex = getRandomArrayItem(startIndexArray);
+
+            var endingIndexJump = randomStartIndex + ((includedOperands - 1) * 2) + 1;
+
+            //check for and fix ending index surpassing array end
+            while (endingIndexJump > expressionArray.length) {
+                //shift both down
+                randomStartIndex -= 2;
+                endingIndexJump -= 2;
+            }
+
+            expressionArray.splice(endingIndexJump, 0, ')');
+            expressionArray.splice(randomStartIndex, 0, '(');
         }
 
-        expressionArray.splice(endingIndexJump, 0, ')');
-        expressionArray.splice(randomStartIndex, 0, '(');
+        return expressionArray;
     }
 
-    return expressionArray;
-}
-
-function prettyPrintExpression(expression){
-    expression = expression.replace(/(\*|\+|-|\/)/g, ' $1 ');
-    return expression;
-}
-
-Array.prototype.juxtapose = function (seperatorFunction) {
-    for(var i = this.length - 1; i > 0; i--){
-        this.splice(i, 0,seperatorFunction());
+    function prettyPrintExpression(expression) {
+        expression = expression.replace(/(\*|\+|-|\/)/g, ' $1 ');
+        return expression;
     }
-    return this;
-}
 
-/*
-expression = expression.replace(/(\(.+\))/g, function(match, contents, offset, s) {
-  //strip out parenthesis
-  match = match.replace(/\(|\)/g, '');
-  var preRPN = match.split(' ');
-return computeExpression(preRPN);
-
-
-});
-console.log(expression);
-var expressionArray = expression.split(' ');
-result = computeExpression(expressionArray);
-console.log(result);
-function computeExpression(expressionArray) {
-expressionArray= reduceOperator(expressionArray, '/');
-expressionArray =reduceOperator(expressionArray, '*');
-expressionArray= reduceOperator(expressionArray, '-');
-expressionArray = reduceOperator(expressionArray, '+');
-return expressionArray.pop();
-}
-
-function reduceOperator(expressionArray, operator) {
-  var operators = ['+', '-', '*', '/'];
-  var operatorType = operators.indexOf(operator);
-
-  var operatorIndex = -1;
-  while ((operatorIndex = expressionArray.indexOf(operator)) > -1) {
-    var operandOne = expressionArray[operatorIndex - 1], operandTwo = expressionArray[operatorIndex + 1], result;
-    
-    switch(operatorType){
-    	case 0:
-      	result = operandOne + operandTwo;
-        break;
-      case 1:
-      result = operandOne - operandTwo;
-        break;
-      case 2:
-      result = operandOne * operandTwo;
-        break;
-      case 3:
-      result = operandOne / operandTwo;
-        break;
+    Array.prototype.juxtapose = function(seperatorFunction) {
+        for (var i = this.length - 1; i > 0; i--) {
+            this.splice(i, 0, seperatorFunction());
+        }
+        return this;
     }
-    
-    expressionArray.splice(operatorIndex - 1, 3, result);
- 
-  }
-  return expressionArray;
-}
-}*/
-function setLevel(level){
-    expressionLevel = level;
-}
-function generateExpression(){
-    var expressionArray = Array.apply(null, {length:expressionLevel}).map(getRandomOperand);
 
-    expressionArray = expressionArray.juxtapose(getRandomOperator);
-    insertRandomParanthesis(expressionArray);
+    function evaluateExpression(expression) {
+        expression = expression.replace(/(\(.+\))/g, function(match, contents, offset, s) {
+            //strip out parenthesis
+            match = match.replace(/\(|\)/g, '');
+            var preRPN = match.split(' ');
+            return computeExpression(preRPN);
 
-    var expression = expressionArray.join('');
-    expression = prettyPrintExpression(expression);
 
-    return expression;
-}
-return {
-    generateExpression: generateExpression
-};
+        });
+        var expressionArray = expression.split(' ');
+        result = computeExpression(expressionArray);
+
+        return result;
+    }
+
+    function computeExpression(expressionArray) {
+        expressionArray = reduceOperator(expressionArray, '/');
+        expressionArray = reduceOperator(expressionArray, '*');
+        expressionArray = reduceOperator(expressionArray, '-');
+        expressionArray = reduceOperator(expressionArray, '+');
+        return expressionArray.pop();
+    }
+
+    function reduceOperator(expressionArray, operator) {
+        var operatorType = operators.indexOf(operator);
+
+        var operatorIndex = -1;
+        while ((operatorIndex = expressionArray.indexOf(operator)) > -1) {
+            //make sure to coerce from string to int
+            var operandOne = +expressionArray[operatorIndex - 1],
+                operandTwo = +expressionArray[operatorIndex + 1],
+                result;
+
+            switch (operatorType) {
+                case 0:
+                    result = operandOne + operandTwo;
+                    break;
+                case 1:
+                    result = operandOne - operandTwo;
+                    break;
+                case 2:
+                    result = operandOne * operandTwo;
+                    break;
+                case 3:
+                    result = operandOne / operandTwo;
+                    break;
+            }
+
+            expressionArray.splice(operatorIndex - 1, 3, result);
+
+        }
+        return expressionArray;
+    }
+
+    function setLevel(level) {
+        expressionLevel = level;
+    }
+
+    function generateExpression() {
+        var expressionArray = Array.apply(null, {
+            length: expressionLevel
+        }).map(getRandomOperand);
+
+        expressionArray = expressionArray.juxtapose(getRandomOperator);
+        insertRandomParanthesis(expressionArray);
+
+        var expression = expressionArray.join('');
+        expression = prettyPrintExpression(expression);
+
+        return expression;
+    }
+
+    return {
+        generateExpression: generateExpression,
+        evaluateExpression: evaluateExpression,
+        setLevel: setLevel
+    };
 })();
--- game.js
+++ game.js
@@ -1,29 +1,60 @@
 #!/usr/bin/env node
 
-checkForLevel(process.argv.join(' '));
 var readline = require('readline');
 var expressionEngine = require('./expression-engine.js');
 
 var readlineInterface = readline.createInterface(process.stdin, process.stdout);
+
+checkForLevel(process.argv.join(' '));
+
 var expression = expressionEngine.generateExpression();
+
 readlineInterface.setPrompt(formatExpressionForQuestion(expression));
 readlineInterface.prompt();
-readlineInterface.on('line', function(line) {
-    if (['quit', 'q'].indexOf(line) > -1) readlineInterface.close();
-    readlineInterface.setPrompt(expressionEngine.generateExpression());
-    readlineInterface.prompt();
-}).on('close',function(){
-    process.exit(0);
-});
 
-function formatExpressionForQuestion(expression){
+readlineInterface.on('line', function(line) {
+        //give a chance for user to quit(besides Ctrl+c)
+        if (['quit', 'q'].indexOf(line) > -1){
+            readlineInterface.close();
+        }
+
+        if(!checkForLevel(line)){
+            var answer = expressionEngine.evaluateExpression(expression);
+            var message = 'Incorrect, the answer is '+ answer;
+            if(answer == line){
+                message = 'Correct!';
+            }
+
+            console.log(message);
+        }
+
+        expression = expressionEngine.generateExpression();
+        readlineInterface.setPrompt(formatExpressionForQuestion(expression));
+        readlineInterface.prompt();
+    }).on('close', function() {
+        process.exit(0);
+    });
+
+//de-couple question format so expression engine can be used for more than just the game
+function formatExpressionForQuestion(expression) {
     return expression + ' = ?';
 }
 
-function checkForLevel(input){
+function checkForLevel(input) {
+    var levelChanged = false;
+
+    //try to match --level anywhere in the string
     var matches = /--level (\d{1})/.exec(input);
-    var level = matches[1];
-    if(level >0 && matches[1] < 6){
-        expressionEngine.setLevel(level);
+    if (matches) {
+        var level = matches[1];
+
+        //set level only if valid int found
+        //otherwise, inside the module, it will default to 3
+        if (level > 0 && matches[1] < 6) {
+            expressionEngine.setLevel(level);
+            levelChanged = true;
+        }
     }
+
+    return levelChanged;
 }
